e300f4f5fdeff1bd51cebcabd39239e9bbe1ebef
abuobaid9 <abuobaid.anas1999@gmail.com>
2023-02-23 11:02:36 +0300

    register & login

---
+++ app/controllers/password_reset_controller.rb
@@ -0,0 +1,33 @@
+class PasswordResetController < ApplicationController
+  def new; end
+
+  def create
+    @user = User.find_by(email: params[:email])
+
+    return unless @user.present?
+
+    PasswordMailer.with(user: @user).reset.deliver_now
+
+    redirect_to root_path,
+                notice: 'If an account with that email was found, we have sent a link to reset your password'
+  end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: 'password_reset')
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: 'Your token has expired. Please try again'
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: 'password_reset')
+    return unless @user.update(password_params)
+
+    redirect_to sign_in_path, notice: 'Your password was reset successfully. please sign in '
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+end
